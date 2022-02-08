01ca57cd268cf686a209099868e3ce4580cb07ab
Daniel ROcha <danielantunesrocha@hotmail.com.br>
2022-02-07 21:12:00 -0300

    Correções de lint

--- js/utils/camera.js
+++ js/utils/camera.js
@@ -16,12 +16,12 @@ export const delay = millis => {
 
 export const startRecording = () => {
   Logger.addLog(`start recording at: ${new Date()}`);
-  return fetch(`http://${CAMARA_IP}/bacpac/SH?t=${CAMARA_PASSWORD}&p=%01`)
+  return fetch(`http://${CAMARA_IP}/bacpac/SH?t=${CAMARA_PASSWORD}&p=%01`);
 };
 
 export const stopRecording = () => {
   Logger.addLog(`stop recording at: ${new Date()}`);
-  return fetch(`http://${CAMARA_IP}/bacpac/SH?t=${CAMARA_PASSWORD}&p=%00`)
+  return fetch(`http://${CAMARA_IP}/bacpac/SH?t=${CAMARA_PASSWORD}&p=%00`);
 };
 
 export const deleteAllFromCamara = () => {
@@ -34,37 +34,54 @@ export const deleteFromCache = filepath => {
   return RNFS.unlink(filepath);
 };
 
-export const deleteVideo = (video) => {
+export const deleteVideo = video => {
   Logger.addLog(`delete video ${video}`);
-  return fetch(`http://${CAMARA_IP}/camera/DF?t=${CAMARA_PASSWORD}&p=%15100GOPRO/${video}`);
+  return fetch(
+    `http://${CAMARA_IP}/camera/DF?t=${CAMARA_PASSWORD}&p=%15113GOPRO/${video}`,
+  );
 };
 
 export const getLastVideoURL = () => {
   Logger.addLog(`fetching video list from camera`);
   return fetch(`http://${CAMARA_IP}:${CAMARA_PORT}/gp/gpMediaList`)
     .then(response => response.json())
-  	.then((jsonResponse) => {
-  		const videoName = _.chain(jsonResponse.media)
-        .find((mediaObj) => mediaObj.d === "100GOPRO")
+    .then(jsonResponse => {
+      const videoName = _.chain(jsonResponse.media)
+        .find(mediaObj => mediaObj.d === '113GOPRO')
         .get('fs')
         .last()
         .get('n')
         .value();
 
-      Logger.addLog(`last video url: http://${CAMARA_IP}:${CAMARA_PORT}/videos/DCIM/100GOPRO/${videoName}`);
-  		return `http://${CAMARA_IP}:${CAMARA_PORT}/videos/DCIM/100GOPRO/${videoName}`;
-  	});
+      Logger.addLog(
+        `last video url: http://${CAMARA_IP}:${CAMARA_PORT}/videos/DCIM/113GOPRO/${videoName}`,
+      );
+      return `http://${CAMARA_IP}:${CAMARA_PORT}/videos/DCIM/113GOPRO/${videoName}`;
+    });
 };
 
 // returns the duration of the video as float
-export const getVideoDuration = (videoURL) => {
+export const getVideoDuration = async videoURL => {
   Logger.addLog(`getting video duration`);
-  return ProcessingManager.getVideoInfo(videoURL)
-    .then(({ duration }) => duration);
+  // getVideoDurationInSeconds(
+  //   'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+  // ).then(duration => {
+  //   console.log(duration);
+  // });
+  // const retorno = await ProcessingManager.getVideoInfo(videoURL).then(
+  //   ({duration}) => duration,
+  // );
+  // console.log({retorno});
+  debugger;
+  return null;
 };
 
 // returns the path of the result file
 export const trimVideo = (videoURL, startTime, endTime) => {
   Logger.addLog(`trimming ${videoURL}, from ${startTime}s to ${endTime}s`);
-  return ProcessingManager.trim(videoURL, { startTime, endTime, saveToCameraRoll: true });
+  return ProcessingManager.trim(videoURL, {
+    startTime,
+    endTime,
+    saveToCameraRoll: true,
+  });
 };
